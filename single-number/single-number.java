860f704de3e02ca4ff95bdfddecef6f0bdc07fe7
jayesh-mali <47940561+jayesh-mali@users.noreply.github.com>
2022-02-15 20:08:57 +0530

    Time: 1 ms, Memory: 49.5 MB - LeetHub

--- single-number/single-number.java
+++ single-number/single-number.java
@@ -1,4 +1,14 @@
 class Solution {
+    
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for(int n: nums){
+            result = result ^ n;
+        }
+        return result;
+    }
+    
+    /*
     public int singleNumber(int[] nums) {
         int result = nums[0];
         for(int i=1; i<nums.length; i++){
@@ -6,4 +16,5 @@ class Solution {
         }
         return result;
     }
+    */
 }
