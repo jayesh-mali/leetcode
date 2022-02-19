d28a63fa35357e3e44a608e73cb1a99530d88e10
jayesh-mali <47940561+jayesh-mali@users.noreply.github.com>
2022-02-19 08:21:26 +0530

    Time: 3 ms, Memory: 54.5 MB - LeetHub

--- move-zeroes/move-zeroes.java
+++ move-zeroes/move-zeroes.java
@@ -9,8 +9,7 @@ class Solution {
                 nums[i-zeroCount] = nums[i];
             }
         }
-        int start = nums.length - zeroCount;
-        for(int i=start; i<nums.length; i++){
+        for(int i=(nums.length - zeroCount); i<nums.length; i++){
             nums[i] = 0;
         }
     }
