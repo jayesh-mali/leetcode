a5c605dcc61d06abde644a2a426b905d19220372
jayesh-mali <47940561+jayesh-mali@users.noreply.github.com>
2022-03-17 10:07:59 +0530

    Time: 1 ms (98.22%), Space: 45 MB (44.13%) - LeetHub

--- 260-single-number-iii/260-single-number-iii.java
+++ 260-single-number-iii/260-single-number-iii.java
@@ -1,8 +1,8 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
-        int xor = nums[0];
-        for(int i=1; i<nums.length; i++){
-            xor ^= nums[i];
+        int xor = 0;
+        for(int num: nums){
+            xor ^= num;
         }
         int lastBit = xor & (~xor+1);
         int sum1 = 0;
